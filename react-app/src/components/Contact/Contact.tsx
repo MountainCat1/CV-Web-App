5b4b7e09b0f82d47a052b286922943c8345d5870
MountainCat <tomasz.banas123@gmail.com>
2022-10-11 15:05:49 +0200

    Added gradient headers

--- react-app/src/components/Contact/Contact.tsx
+++ react-app/src/components/Contact/Contact.tsx
@@ -4,6 +4,10 @@ export default function (){
 
 
     return(<>
+        <h1 className='gradient-text'>
+            Contact
+        </h1>
+
         This is contact!
     </>)
 }
--- react-app/src/components/Tickets/Tickets.tsx
+++ react-app/src/components/Tickets/Tickets.tsx
@@ -6,6 +6,10 @@ export default function (){
 
 
     return(<>
+        <h1 className='gradient-text'>
+            Tickets
+        </h1>
+
         Here will be some tickets!
     </>)
 }
