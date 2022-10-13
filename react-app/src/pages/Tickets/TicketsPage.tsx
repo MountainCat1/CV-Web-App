7926ddcc7fd2a2e98669a1f58d9d6cd214754ba7
MountainCat <tomasz.banas123@gmail.com>
2022-10-13 05:30:41 +0200

    added cards

--- react-app/src/pages/Tickets/TicketsPage.tsx
+++ react-app/src/pages/Tickets/TicketsPage.tsx
@@ -12,21 +12,43 @@ export default function (){
 
         <p>Here will be some tickets!</p>
 
-        <div className="card-container">
-            <div className='card'>
-                <img
-                    alt='nice man'
-                    className='card-image'
-                    src='/img/photos/48502301-view-of-a-young-attractive-man-working-in-a-call-center.jpg'/>
-                Card A
-            </div>
-            <div className='card'>
-                Card B
-            </div>
-            <div className='card'>
-                Card C
-            </div>
+        {/*<div className='gradient-border'>*/}
+            <div className="card-container gradient-border-content">
+                <div className='card'>
+                    <div>
+                        <div className='card-content'>
+                            <h2 className='card-title'>
+                                Mega Tickets
+                            </h2>
+                            <p className='card-body'>
+                                It's super mega cooool ticket. You want it! :D
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div className='card-content'>
+                        <h2 className='card-title'>
+                            Mega Tickets
+                        </h2>
+                        <p className='card-body'>
+                            It's super mega cooool ticket. You want it! :D
+                        </p>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div className='card-content'>
+                        <h2 className='card-title'>
+                            Mega Tickets
+                        </h2>
+                        <p className='card-body'>
+                            It's super mega cooool ticket. You want it! :D
+                        </p>
+                    </div>
+                </div>
+            {/*</div>*/}
         </div>
+
 
     </div>)
 }
