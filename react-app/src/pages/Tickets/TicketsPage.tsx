1abdb0e2ee2113795c954c195d6a2249c43aa61f
MountainCat <tomasz.banas123@gmail.com>
2022-10-13 05:32:48 +0200

    added gradient border to card container

--- react-app/src/pages/Tickets/TicketsPage.tsx
+++ react-app/src/pages/Tickets/TicketsPage.tsx
@@ -12,21 +12,9 @@ export default function (){
 
         <p>Here will be some tickets!</p>
 
-        {/*<div className='gradient-border'>*/}
+        <div className='gradient-border'>
             <div className="card-container gradient-border-content">
                 <div className='card'>
-                    <div>
-                        <div className='card-content'>
-                            <h2 className='card-title'>
-                                Mega Tickets
-                            </h2>
-                            <p className='card-body'>
-                                It's super mega cooool ticket. You want it! :D
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className='card'>
                     <div className='card-content'>
                         <h2 className='card-title'>
                             Mega Tickets
@@ -46,7 +34,17 @@ export default function (){
                         </p>
                     </div>
                 </div>
-            {/*</div>*/}
+                <div className='card'>
+                    <div className='card-content'>
+                        <h2 className='card-title'>
+                            Mega Tickets
+                        </h2>
+                        <p className='card-body'>
+                            It's super mega cooool ticket. You want it! :D
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
