2e57ea93c49eb29e8d5a99e8a8b292f997e3a7f4
Tomasz Banaś <72751263+MountainCat1@users.noreply.github.com>
2022-10-15 10:21:24 +0200

    Added some basic contact page

--- react-app/src/pages/Contact/ContactPage.tsx
+++ react-app/src/pages/Contact/ContactPage.tsx
@@ -1,4 +1,4 @@
-
+import './ContactPage.css'
 
 export default function (){
 
@@ -9,5 +9,25 @@ export default function (){
         </h1>
 
         <p>This is contact!</p>
+
+        <div className={'gradient-border'}>
+            <div className="gradient-border-content">
+                <div className='contact-info-container'>
+                    <div className='contact-info'>
+                        <p>Email:</p>
+                        <p>someEmail@gmail.com</p>
+                    </div>
+                    <div className='contact-info'>
+                        <p>Phone:</p>
+                        <p>48+ 660 660 660</p>
+                    </div>
+                    <div className='contact-info'>
+                        <p>Discord:</p>
+                        <p>YourMom#2137</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>)
 }
