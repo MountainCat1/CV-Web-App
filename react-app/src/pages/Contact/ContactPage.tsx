2123dee5ff4f20fcafbfbd1702be20436f365c89
Tomasz Banaś <72751263+MountainCat1@users.noreply.github.com>
2022-10-15 11:31:26 +0200

    Added icons for ContactPage

--- react-app/src/pages/Contact/ContactPage.tsx
+++ react-app/src/pages/Contact/ContactPage.tsx
@@ -14,15 +14,15 @@ export default function (){
             <div className="gradient-border-content">
                 <div className='contact-info-container'>
                     <div className='contact-info'>
-                        <p>Email:</p>
+                        <img className={'contact-image'} src='/img/icon/mail.png' alt={'Email icon'}/>
                         <p>someEmail@gmail.com</p>
                     </div>
                     <div className='contact-info'>
-                        <p>Phone:</p>
+                        <img className={'contact-image'} src='/img/icon/telephone.png' alt={'Phone icon'}/>
                         <p>48+ 660 660 660</p>
                     </div>
                     <div className='contact-info'>
-                        <p>Discord:</p>
+                        <img className={'contact-image'} src='/img/icon/discord.png' alt={'Discord icon'}/>
                         <p>YourMom#2137</p>
                     </div>
                 </div>
