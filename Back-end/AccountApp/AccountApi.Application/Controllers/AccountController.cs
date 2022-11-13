71494c4811067965adda606875db54d684895cf0
tbanas <Tomasz.Banas@finture.com>
2022-11-13 13:19:40 +0100

    refactoring, fixed naming

--- Back-end/AccountApp/AccountApi.Application/Controllers/AccountController.cs
+++ Back-end/AccountApp/AccountApi.Application/Controllers/AccountController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AccountApp.Controllers
+namespace AccountApi.Controllers
 {
     [Route("api/account")]
     [ApiController]
