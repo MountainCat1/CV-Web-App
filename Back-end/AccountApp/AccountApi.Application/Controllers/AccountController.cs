f5ffdcbf862a8a468abc4d1a7b45523cda52ad93
tbanas <Tomasz.Banas@finture.com>
2022-11-13 13:24:10 +0100

    added base for CreateAccount endpoint

--- Back-end/AccountApp/AccountApi.Application/Controllers/AccountController.cs
+++ Back-end/AccountApp/AccountApi.Application/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AccountApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,12 @@ namespace AccountApi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateAccount([FromBody] CreateAccountModel model)
+        {
+            // TODO make this endpoint create an account
+            return Ok();
+        }
     }
 }
