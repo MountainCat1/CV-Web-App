23e408d1b1f8077de233f98ff693d03dbb319675
MountainCat <tomasz.banas123@gmail.com>
2022-11-29 00:33:48 +0100

    changes to grind in MainPage.tsx

--- react-app/src/pages/Main/MainPage.tsx
+++ react-app/src/pages/Main/MainPage.tsx
@@ -3,7 +3,7 @@ import Reveal from "../../components/Reveal/Reveal";
 
 
 export default function () {
-    return (<div className={'page-content'}>
+    return (<span className={'page-content'}>
         <h1>Tomasz Banaś</h1>
         <p>
             Welcome to my amazing website!
@@ -15,48 +15,31 @@ export default function () {
 
         <div className='gradient-border'>
             <div className='gradient-border-content'>
-
                 <Reveal className="text-with-image" effect={'revealed'} offset={0}>
                     <img className='inline-image'
                          src='/img/photos/ME_PC.jpg'
                          alt={'young-attractive-man-working-in-a-call-center'}/>
                     <p>Me at my PC doing some coooding</p>
-                    {/*
-                    <Reveal offset={150} effect='revealed'>
-                    </Reveal>*/}
                 </Reveal>
             </div>
         </div>
 
         <span className={'main-page-text'}>
-            <Reveal effect={'revealed'} offset={170}>
-                <p>
-                    I am a passionate programmer who tries to develop towards Web Development, mainly Back-End, aswell as
-                    Game Dev. I am happy to learn about new solutions and technology to improve myself in the field of IT.
-                </p>
-            </Reveal>
-            <Reveal effect={'revealed'} offset={100}>
-                <p>
-                    I have great enthusiasm for learning. I try as much as I can to make my skills constantly expand.
-                </p>
-            </Reveal>
-            <Reveal effect={'revealed'} offset={170}>
-                <p>
-                    I am studying and working in order to gain experience and practical skills. Like every student, I have
-                    my
-                    shortcomings, but i easly make up for it with my commitment and determination. I have no problem
-                    listening
-                    and learning from the more experienced team members. I understand that there is still a long way to go,
-                    on
-                    the programmer's path.
-                </p>
-            </Reveal>
-            <Reveal effect={'revealed'} offset={100}>
-                <p>
-                    As a member of the team, I am very cheerful, and I can introduce a looser and pleasant atmosphere for
-                    everyone.
-                </p>
-            </Reveal>
+            <div className={'grid-item-left'}>
+                <Reveal effect={'revealed'} offset={100}>
+                    <p className={'grid-item-text'}>
+                        I am a passionate programmer who tries to develop towards Web Development, mainly Back-End, as well as
+                        Game Dev. I am happy to learn about new solutions and technology to improve myself in the field of IT.
+                    </p>
+                </Reveal>
+            </div>
+            <div className={'grid-item-right'}>
+                <Reveal effect={'revealed'} offset={100}>
+                    <p className={'grid-item-text'}>
+                        I have great enthusiasm for learning. I try as much as I can to make my skill set constantly expand.
+                    </p>
+                </Reveal>
+            </div>
         </span>
 
         <div className='gradient-border'>
@@ -66,28 +49,43 @@ export default function () {
                     <img className='inline-image'
                          src='/img/photos/DimWarper.png'
                          alt={'young-attractive-man-working-in-a-call-center'}/>
-                    <p>My game Dimwarper avaliable on Steam!</p>
-                    {/*
-                    <Reveal offset={150} effect='revealed'>
-                    </Reveal>*/}
+                    <p>My game Dimwarper available on Steam!</p>
                 </Reveal>
             </div>
         </div>
 
+        <span className={'main-page-text'}>
+            <div className={'grid-item-left'}>
+                <Reveal effect={'revealed'} offset={100}>
+                    <p className={'grid-item-text'}>
+                        I am studying and working in order to gain experience and practical skills. I have no problem
+                        listening
+                        and learning from the more experienced team members. I understand that there is still a long way to go,
+                        on
+                        the programmer's path.
+                    </p>
+                </Reveal>
+            </div>
+            <div className={'grid-item-right'}>
+                <Reveal effect={'revealed'} offset={100}>
+                    <p className={'grid-item-text'}>
+                        As a member of the team, I am very cheerful, and I can introduce a looser and more pleasant atmosphere for
+                        everyone.
+                    </p>
+                </Reveal>
+            </div>
+        </span>
+
         <div className='gradient-border'>
             <div className='gradient-border-content'>
-
                 <Reveal className="text-with-image" effect={'revealed'} offset={50}>
                     <img className='inline-image'
                          src='/img/photos/ME-STUPID.jpg'
                          alt={'young-attractive-man-working-in-a-call-center'}/>
                     <p>Me at my college studying applied computer science!</p>
-                    {/*
-                    <Reveal offset={150} effect='revealed'>
-                    </Reveal>*/}
                 </Reveal>
             </div>
         </div>
 
-    </div>)
+    </span>)
 }
