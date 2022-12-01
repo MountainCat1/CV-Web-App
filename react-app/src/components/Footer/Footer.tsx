9f1d2ba8924fda863d40dfb923a0303b82c5b590
tbanas <Tomasz.Banas@finture.com>
2022-12-01 20:38:47 +0100

    changed footer layout, added copyright info

--- react-app/src/components/Footer/Footer.tsx
+++ react-app/src/components/Footer/Footer.tsx
@@ -1,15 +1,24 @@
 import './Footer.css'
+import {useState} from "react";
+import {NavLink} from "react-router-dom";
 
 export default function (){
+    const [date, ] = useState(new Date())
+
     return (<div className={'footer'}>
         <div>
-            Footer
+            CHANGE IT PLEASEEEE
         </div>
         <div>
-            Footer
+            <NavLink
+                to={('/contact')}>
+                Contact
+            </NavLink>
         </div>
         <div>
-            Footer
+            <p>
+                © {date.getFullYear()} Tomasz Banaś
+            </p>
         </div>
     </div>)
 }
