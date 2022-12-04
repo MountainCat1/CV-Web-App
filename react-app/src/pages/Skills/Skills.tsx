d2a979c59dd9760e539225e18065b944228574d0
MountainCat <tomasz.banas123@gmail.com>
2022-12-04 13:55:02 +0100

    addes skills page

--- react-app/src/pages/Skills/Skills.tsx
+++ react-app/src/pages/Skills/Skills.tsx
@@ -1,5 +1,59 @@
+import './Skills.css'
 
+export default function () {
+    return (<>
+        <h2>Back-End</h2>
+        <div>
+            <h3>.Net/.Net Core</h3>
 
-export default function (){
-    return (<></>)
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad dolores eius eos ex ipsam mollitia nihil
+            quis. Amet aut dolorem enim maiores, pariatur quae rerum sunt tenetur velit voluptas.
+        </div>
+        <div>
+            <h3>.ASP.NET</h3>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolores ex explicabo, illo
+            necessitatibus porro, possimus repellendus sed sunt suscipit tenetur ut. Eum explicabo incidunt iste,
+            quisquam repellat suscipit?
+        </div>
+        <div>
+            <h3>Azure</h3>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis cupiditate dignissimos
+            dolorum earum, eligendi excepturi exercitationem fugit itaque laborum laudantium minima nam non quam qui
+            reiciendis, sequi, soluta vel.
+        </div>
+        <div>
+            <h3>Git</h3>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis cupiditate dignissimos
+            dolorum earum, eligendi excepturi exercitationem fugit itaque laborum laudantium minima nam non quam qui
+            reiciendis, sequi, soluta vel.
+        </div>
+        <div>
+            <h3>SQL</h3>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis cumque cupiditate debitis
+            dicta earum inventore ipsam iste iure labore molestias neque nisi non provident qui, sed sint sit
+            voluptatum.
+        </div>
+
+        <h2>Front-End</h2>
+
+        <div>
+            <h3>TypeScript</h3>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis cumque cupiditate debitis
+            dicta earum inventore ipsam iste iure labore molestias neque nisi non provident qui, sed sint sit
+            voluptatum.
+        </div>
+        <div>
+            <h3>React TS</h3>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias autem, corporis dolorem eaque eum
+            explicabo facere fuga in, ipsa labore maiores molestiae necessitatibus nihil qui quod recusandae vel
+            voluptatibus!
+        </div>
+
+    </>)
 }
